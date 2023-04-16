da147b8042a5d8c5125e085b2499017a87f1cbd6
Marco Ollivier <marcopollivier@gmail.com>
2023-04-16 17:27:19 -0300

    add new changes

--- index.js
+++ index.js
@@ -4,23 +4,70 @@ const { create } = require('xmlbuilder2')
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const convertWithXPath = () => {
+  return create({ version: '1.0' })
+  .ele('soap:Envelope', { 'xmlns:soap': 'http://schemas.xmlsoap.org/soap/envelope/', 
+                          'xmlns:soapenc': 'http://schemas.xmlsoap.org/soap/encoding/',
+                          'xmlns:tns': 'http://wms2.marcopollivier.dev/soap/webservice.xpto',
+                        
+                          'xmlns:types': 'http://wms2.marcopollivier.dev/soap/webservice.xpto/encodedTypes',
+                          'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+                          'xmlns:xsd': 'http://www.w3.org/2001/XMLSchema',
+                        })
+    .ele('soap:Body', { 'id': '#1', 'soapenc:arrayType': "tns:Livro[1]" })
+  .up();
+}
+
+const convertWithJSON = () => {
+  const obj = {
+    'soap:Envelope': {
+      '@xmlns:soap': "http://schemas.xmlsoap.org/soap/envelope/",
+      '@xmlns:soapenc': "http://schemas.xmlsoap.org/soap/encoding/",
+      '@xmlns:tns': "http://wms2.marcopollivier.dev/soap/webservice.xpto",
+      '@xmlns:types': "http://wms2.marcopollivier.dev/soap/webservice.xpto/encodedTypes",
+      '@xmlns:xsi': "http://www.w3.org/2001/XMLSchema-instance",
+      '@xmlns:xsd': "http://www.w3.org/2001/XMLSchema",
+
+      'soap:Body': {
+        '@soap:encodingStyle': 'http://schemas.xmlsoap.org/soap/encoding/',
+
+        'tns:CadastraLivros': {
+          '@id': 'id1',
+          '@xsi:type': 'tns:CadastraLivros',
+          
+          'LivrosArray': {
+            '@href': '#id2'
+          }
+
+        },
+
+        'soapenc:Array': {
+          '@id': 'id2',
+          '@soapenc:arrayType': 'tns:Livro[1]',
+
+          'Item': { '@href': '#id3' }
+        },
+      }
+
+    }
+  };
+
+  const original = create(obj)
+
+  original.root().ele('soap:Body').ele('aaa')
+  
+
+  return original
+}
+
 const server = http.createServer((req, res) => {
 
-  const body = create({ version: '1.0' })
-    .ele('base', { att: 'att_base_1', att2: 'att_base_2' })
-      
-      .ele('niv1_a')
-        .ele('niv2_a', { att: 'att_base_1', att2: 'att_base_2' }).txt('Estou no niv 2').up()
-        .ele('niv2_b').txt('Ainda no niv 2').up()
-      .up()
-
-      .ele('ni1_b').up()
-    .up();
-
-  const complete = body.root().ele('niv1_a').ele('aaaa').att(false)
+  // const body = convertWithXPath();
+  const body = convertWithJSON();
   
-  // convert the XML tree to string
-  const xml = complete.end({ prettyPrint: true });
+  const document = body
+  
+  const xml = document.end({ prettyPrint: true });
   console.log(xml);
 
   res.statusCode = 200;
